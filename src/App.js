f3c1d14b1f3d8f8d7a83f91d57f705d053cb7010
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2022-02-17 19:07:47 -0800

    Added Data from Data.json to a state called planets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
+import {useState} from 'react'
 import Header from "./MainComponents/Header";
+import Data from './starter-code/data.json'
 
 function App() {
+  const [planets, setPlanets] = useState(Data)
+  
   return (
     <div className="container">
       <Header />
