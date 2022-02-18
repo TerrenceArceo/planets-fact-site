3d2157de754de58af6cebbf73b2b1acb5f9e7156
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2022-02-17 18:46:55 -0800

    Installed react-router-dom and wrapped App component with BrowserRouter as Router

--- src/MainComponents/Header.js
+++ src/MainComponents/Header.js
@@ -2,9 +2,9 @@ import hamburger from '../starter-code/assets/icon-hamburger.svg'
 
 export default function Header() {
     return (
-        <div className='header'>
-            <h1>THE PLANETS</h1>
+        <nav className='header'>
+            <h2>THE PLANETS</h2>
             <img src={hamburger} alt='hamburger'/>
-        </div>
+        </nav>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import style from './style.css'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <App />
+  </Router>,
   document.getElementById('root')
 );
