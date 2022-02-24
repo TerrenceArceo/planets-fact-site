311fe5723ddfe8c093d0625ac917ac8a2bb2d2cd
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2022-02-23 19:57:25 -0800

    completed mobile layout for main body

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import {useState} from 'react'
 import Header from "./MainComponents/Header";
+import Body from './MainComponents/Body'
 import Data from './starter-code/data.json'
 
 function App() {
   const [planets, setPlanets] = useState(Data)
-  
+
   return (
     <div className="container">
       <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/MainComponents/Body.js
@@ -0,0 +1,19 @@
+import source from '../starter-code/assets/icon-source.svg'
+
+export default function Body() {
+    return (
+        <div className="main-body">
+            <div className="info-buttons">
+                <button>Overview</button>
+                <button>Structure</button>
+                <button>Surface</button>
+            </div>
+            <img src="#" alt="mercury" className="img-planet"/>
+            <div className="info">
+                <h1 className="title">Mercury</h1>
+                <p className="description">Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terres trial planets in the Solar System, and is a rocky body like Earth.</p>
+                <p className="description">Source : <a href="#" className="wiki-link">Wikipedia</a> <img src={source}/></p>
+            </div>
+        </div>
+    )
+}
