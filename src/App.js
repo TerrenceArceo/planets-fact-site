58eaf5653c44e22649b79bc1cc241c2031d0d132
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2022-02-24 20:22:55 -0800

    created and styled footer info

--- src/App.js
+++ src/App.js
@@ -16,3 +16,8 @@ function App() {
 }
 
 export default App;
+
+
+// we will try to use props.children and we will only have one body.
+// and then to avoid repetition of code, we will create a single function that handles all of that, that takes in a parameter which is the index
+// so everytime we press a different planet, we will input the index of that planet and re-render the body.
--- src/MainComponents/Body.js
+++ src/MainComponents/Body.js
@@ -15,6 +15,24 @@ export default function Body() {
                 <p className="description">Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terres trial planets in the Solar System, and is a rocky body like Earth.</p>
                 <p className="description">Source : <a href="#" className="wiki-link">Wikipedia</a> <img src={source}/></p>
             </div>
+            <div className='footer'>
+                <div className='footer-info'>
+                    <p>rotation time</p>
+                    <h3>58.6 days</h3>
+                </div>
+                <div className='footer-info'>
+                    <p>revolution time</p>
+                    <h3>87.97 days</h3>
+                </div>
+                <div className='footer-info'>
+                    <p>radius</p>
+                    <h3>2,439.7 km</h3>
+                </div>
+                <div className='footer-info'>
+                    <p>average temp.</p>
+                    <h3>430°c</h3>
+                </div>
+            </div>
         </div>
     )
 }
