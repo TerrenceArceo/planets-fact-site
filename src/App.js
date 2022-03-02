f955682e0f24d1e1305f88c2a8fc3a78e0965c79
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2022-03-01 20:58:16 -0800

    imported Routes and Route from React router and moved the body JSX inside the App component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,71 @@
 
 import {useState} from 'react'
+import { Routes, Route } from 'react-router-dom'
 import Header from "./MainComponents/Header";
 import Body from './MainComponents/Body'
 import Data from './starter-code/data.json'
+import source from './starter-code/assets/icon-source.svg'
+
+import Earth from './MainComponents/planets/Earth'
+import Jupiter from './MainComponents/planets/Jupiter'
+import Mars from './MainComponents/planets/Mars'
+import Mercury from './MainComponents/planets/Mercury'
+import Neptune from './MainComponents/planets/Neptune'
+import Saturn from './MainComponents/planets/Saturn'
+import Uranus from './MainComponents/planets/Uranus'
+import Venus from './MainComponents/planets/Venus'
 
 function App() {
   const [planets, setPlanets] = useState(Data)
 
+  function body() {
+    return (
+      <div className="main-body">
+            <div className="info-buttons">
+                <button>Overview</button>
+                <button>Structure</button>
+                <button>Surface</button>
+            </div>
+            <img src="#" alt="mercury" className="img-planet"/>
+            <div className="info">
+                <h1 className="title">Mercury</h1>
+                <p className="description">Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terres trial planets in the Solar System, and is a rocky body like Earth.</p>
+                <p className="description">Source : <a href="#" className="wiki-link">Wikipedia</a> <img src={source}/></p>
+            </div>
+            <div className='footer'>
+                <div className='footer-info'>
+                    <p>rotation time</p>
+                    <h3>58.6 days</h3>
+                </div>
+                <div className='footer-info'>
+                    <p>revolution time</p>
+                    <h3>87.97 days</h3>
+                </div>
+                <div className='footer-info'>
+                    <p>radius</p>
+                    <h3>2,439.7 km</h3>
+                </div>
+                <div className='footer-info'>
+                    <p>average temp.</p>
+                    <h3>430°c</h3>
+                </div>
+            </div>
+        </div>
+    )
+  }
+
   return (
     <div className="container">
       <Header />
-      <Body />
+
+      <Routes>
+        <Route 
+          path="/" 
+          element={ <Mercury>{body()}</Mercury>}
+        />
+      </Routes>
+
+
     </div>
   );
 }
