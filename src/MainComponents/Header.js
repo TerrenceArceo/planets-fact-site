e61b810873036a711835d721b1f72683c44d5e05
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2022-04-05 19:52:50 -0700

    added useWindowSize custom hook to keep track of the window size and use it for navigation's conditional rendering

--- src/MainComponents/Header.js
+++ src/MainComponents/Header.js
@@ -1,14 +1,18 @@
 // import hamburger from '/assets/icon-hamburger.svg'
 
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
+import useWindowSize from './useWindowSize'
 
 export default function Header(props) {
     const [isShown, setIsShown] = useState(false)
+    const size = useWindowSize()
 
     function toggleNav() {
         setIsShown(prevState => !prevState)
     }
+
+    
 
     return (
         <nav className='header'>
@@ -16,48 +20,48 @@ export default function Header(props) {
                 <h2>THE PLANETS</h2>
                 <img src="/assets/icon-hamburger.svg" alt='hamburger' className="hamburger" onClick={toggleNav}/>
             </div>
-            {isShown && <ul className="header-navigation-planets">
-                <li className='planet'>
-                    <span className='mercury-dot'></span>
-                    <Link to="/">Mercury</Link>
-                    <img src='/assets/icon-chevron.svg' className='chevron' />
-                </li>
-                <li className='planet'>
-                    <span className='venus-dot'></span>
-                    <Link to="Venus">Venus</Link>
-                    <img src='/assets/icon-chevron.svg' className='chevron' />
-                </li>
-                <li className='planet'>
-                    <span className='earth-dot'></span>
-                    <Link to="Earth">Earth</Link>
-                    <img src='/assets/icon-chevron.svg' className='chevron' />
-                </li>
-                <li className='planet'>
-                    <span className='mars-dot'></span>
-                    <Link to="Mars">Mars</Link>
-                    <img src='/assets/icon-chevron.svg' className='chevron' />
-                </li>
-                <li className='planet'>
-                    <span className='jupiter-dot'></span>
-                    <Link to="Jupiter">Jupiter</Link>
-                    <img src='/assets/icon-chevron.svg' className='chevron' />
-                </li>
-                <li className='planet'>
-                    <span className='saturn-dot'></span>
-                    <Link to="Saturn">Saturn</Link>
-                    <img src='/assets/icon-chevron.svg' className='chevron' />
-                </li>
-                <li className='planet'>
-                    <span className='uranus-dot'></span>
-                    <Link to="Uranus">Uranus</Link>
-                    <img src='/assets/icon-chevron.svg' className='chevron' />
-                </li>
-                <li className='planet'>
-                    <span className='neptune-dot'></span>
-                    <Link to="Neptune">Neptune</Link>
-                    <img src='/assets/icon-chevron.svg' className='chevron' />
-                </li>
-            </ul>}
+            {isShown || size.width >= 480 ? <ul className="header-navigation-planets">
+                    <li className='planet'>
+                        <span className='mercury-dot'></span>
+                        <Link to="/">Mercury</Link>
+                        <img src='/assets/icon-chevron.svg' className='chevron' />
+                    </li>
+                    <li className='planet'>
+                        <span className='venus-dot'></span>
+                        <Link to="Venus">Venus</Link>
+                        <img src='/assets/icon-chevron.svg' className='chevron' />
+                    </li>
+                    <li className='planet'>
+                        <span className='earth-dot'></span>
+                        <Link to="Earth">Earth</Link>
+                        <img src='/assets/icon-chevron.svg' className='chevron' />
+                    </li>
+                    <li className='planet'>
+                        <span className='mars-dot'></span>
+                        <Link to="Mars">Mars</Link>
+                        <img src='/assets/icon-chevron.svg' className='chevron' />
+                    </li>
+                    <li className='planet'>
+                        <span className='jupiter-dot'></span>
+                        <Link to="Jupiter">Jupiter</Link>
+                        <img src='/assets/icon-chevron.svg' className='chevron' />
+                    </li>
+                    <li className='planet'>
+                        <span className='saturn-dot'></span>
+                        <Link to="Saturn">Saturn</Link>
+                        <img src='/assets/icon-chevron.svg' className='chevron' />
+                    </li>
+                    <li className='planet'>
+                        <span className='uranus-dot'></span>
+                        <Link to="Uranus">Uranus</Link>
+                        <img src='/assets/icon-chevron.svg' className='chevron' />
+                    </li>
+                    <li className='planet'>
+                        <span className='neptune-dot'></span>
+                        <Link to="Neptune">Neptune</Link>
+                        <img src='/assets/icon-chevron.svg' className='chevron' />
+                    </li>
+                </ul> : null}
         </nav>
     )
 }
