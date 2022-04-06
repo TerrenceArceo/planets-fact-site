0b17f418e82821f108be7ba17e4ba8756c479462
TerrenceArceo <76758014+TerrenceArceo@users.noreply.github.com>
2022-04-05 21:23:56 -0700

    adding a function to each of mobile navigation planets that will set isShown to false, so when a planet is clicked; the navigation will automatically close

--- src/MainComponents/Header.js
+++ src/MainComponents/Header.js
@@ -21,42 +21,42 @@ export default function Header(props) {
                 <img src="/assets/icon-hamburger.svg" alt='hamburger' className="hamburger" onClick={toggleNav}/>
             </div>
             {isShown || size.width >= 480 ? <ul className="header-navigation-planets">
-                    <li className='planet'>
+                    <li className='planet' onClick={() => setIsShown(false)}>
                         <span className='mercury-dot'></span>
                         <Link to="/">Mercury</Link>
                         <img src='/assets/icon-chevron.svg' className='chevron' />
                     </li>
-                    <li className='planet'>
+                    <li className='planet' onClick={() => setIsShown(false)}>
                         <span className='venus-dot'></span>
                         <Link to="Venus">Venus</Link>
                         <img src='/assets/icon-chevron.svg' className='chevron' />
                     </li>
-                    <li className='planet'>
+                    <li className='planet' onClick={() => setIsShown(false)}>
                         <span className='earth-dot'></span>
                         <Link to="Earth">Earth</Link>
                         <img src='/assets/icon-chevron.svg' className='chevron' />
                     </li>
-                    <li className='planet'>
+                    <li className='planet' onClick={() => setIsShown(false)}>
                         <span className='mars-dot'></span>
                         <Link to="Mars">Mars</Link>
                         <img src='/assets/icon-chevron.svg' className='chevron' />
                     </li>
-                    <li className='planet'>
+                    <li className='planet' onClick={() => setIsShown(false)}>
                         <span className='jupiter-dot'></span>
                         <Link to="Jupiter">Jupiter</Link>
                         <img src='/assets/icon-chevron.svg' className='chevron' />
                     </li>
-                    <li className='planet'>
+                    <li className='planet' onClick={() => setIsShown(false)}>
                         <span className='saturn-dot'></span>
                         <Link to="Saturn">Saturn</Link>
                         <img src='/assets/icon-chevron.svg' className='chevron' />
                     </li>
-                    <li className='planet'>
+                    <li className='planet' onClick={() => setIsShown(false)}>
                         <span className='uranus-dot'></span>
                         <Link to="Uranus">Uranus</Link>
                         <img src='/assets/icon-chevron.svg' className='chevron' />
                     </li>
-                    <li className='planet'>
+                    <li className='planet' onClick={() => setIsShown(false)}>
                         <span className='neptune-dot'></span>
                         <Link to="Neptune">Neptune</Link>
                         <img src='/assets/icon-chevron.svg' className='chevron' />
